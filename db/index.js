ad8f9cb511714e548280b66dcba3b2603258f073
MonkComputer <bmonk1996@yahoo.com>
2023-03-02 09:30:04 -0600

    finished pat 2

--- db/index.js
+++ db/index.js
@@ -1,99 +1,161 @@
-const { Client } = require('pg');
-const client = new Client('postgres://localhost:5432/juicebox-dev');
+const { Client } = require("pg");
+const client = new Client("postgres://localhost:5432/juicebox-dev");
 
 async function getAllUsers() {
-    const { rows } = await client.query(
-      `SELECT id, username, name, location, active
+  const { rows } = await client.query(
+    `SELECT id, username, name, location, active
       FROM users;
-    `);
-  
-    return rows;
-  }
+    `
+  );
 
-  async function createUser({ username, password, name, location }) {
-    try {
-        const { rows: [ user ] } = await client.query(`INSERT INTO users(username, password, name, location)
+  return rows;
+}
+
+async function createUser({ username, password, name, location }) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `INSERT INTO users(username, password, name, location)
       VALUES ($1, $2, $3, $4)
       ON CONFLICT(username)DO NOTHING
       RETURNING *;
-    `, [username, password, name, location]);
-  
-      return user
-    } catch (error) {
-      throw error;
-    }
+    `,
+      [username, password, name, location]
+    );
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function updateUser(id, fields = {}) {
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(", ");
+
+  if (setString.length === 0) {
+    return;
   }
 
-  async function updateUser(id, fields = {}) {
-    
-    const setString = Object.keys(fields).map(
-      (key, index) => `"${ key }"=$${ index + 1 }`
-    ).join(', ');
-  
-    
-    if (setString.length === 0) {
-      return;
-    }
-  
-    try {
-        const { rows: [ user ] } = await client.query(`
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
         UPDATE users
-        SET ${ setString }
-        WHERE id=${ id }
+        SET ${setString}
+        WHERE id=${id}
         RETURNING *;
-      `, Object.values(fields));
-  
-      return user;
-    } catch (error) {
-      throw error;
-    }
+      `,
+      Object.values(fields)
+    );
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function createPost({ authorId, title, content }) {
+  try {
+    const {
+      rows: [post],
+    } = await client.query(
+      `INSERT INTO posts("authorId", title, content)
+      VALUES ($1, $2, $3)
+      RETURNING *;
+    `,
+      [authorId, title, content]
+    );
+
+    return post;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function updatePost(id, fields = {}) {
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(", ");
+
+  if (setString.length === 0) {
+    return;
   }
 
-  async function createPost({
-    authorId,
-    title,
-    content
-  }) {
-    try {
-  
-    } catch (error) {
-      throw error;
-    }
-  }
+  try {
+    const {
+      rows: [post],
+    } = await client.query(
+      `
+        UPDATE posts
+        SET ${setString}
+        WHERE id=${id}
+        RETURNING *;
+      `,
+      Object.values(fields)
+    );
 
-  async function updatePost(id, {
-    title,
-    content,
-    active
-  }) {
-    try {
-  
-    } catch (error) {
-      throw error;
-    }
+    return post;
+  } catch (error) {
+    throw error;
   }
+}
 
-  async function getAllPosts() {
-    try {
-  
-    } catch (error) {
-      throw error;
-    }
-  }
+async function getAllPosts() {
+  const { rows } = await client.query(
+    `SELECT * FROM posts;
+    `
+  );
 
-  async function getPostsByUser(userId) {
-    try {
-      const { rows } = await client.query(`
+  return rows;
+}
+
+async function getPostsByUser(userId) {
+  try {
+    const { rows } = await client.query(`
         SELECT * FROM posts
-        WHERE "authorId"=${ userId };
+        WHERE "authorId"=${userId};
       `);
-  
-      return rows;
-    } catch (error) {
-      throw error;
-    }
+
+    return rows;
+  } catch (error) {
+    throw error;
   }
+}
 
+async function getUserById(userId) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(`
+        SELECT * FROM users
+        WHERE "id"=${userId};
+      `);
+    if (!user) {
+      return null;
+    }
+    delete user.password;
 
+    myPosts = await getPostsByUser(userId);
+    user.posts = myPosts;
 
-  module.exports = {client, createUser, getAllUsers, updateUser, createPost, updatePost, getAllPosts,getPostsByUser}
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = {
+  client,
+  createUser,
+  getAllUsers,
+  updateUser,
+  createPost,
+  updatePost,
+  getAllPosts,
+  getPostsByUser,
+  getUserById,
+};
--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,14 @@
-const { client, getAllUsers, createUser, updateUser, createPost } = require("./index");
+const {
+  client,
+  createUser,
+  getAllUsers,
+  updateUser,
+  createPost,
+  updatePost,
+  getAllPosts,
+  getPostsByUser,
+  getUserById,
+} = require("./index");
 
 async function dropTables() {
   try {
@@ -31,7 +41,7 @@ async function createTables() {
         );
       `);
 
-      await client.query(`
+    await client.query(`
         CREATE TABLE posts (
           id SERIAL PRIMARY KEY,
           "authorId" INTEGER REFERENCES users(id) NOT NULL,
@@ -50,58 +60,128 @@ async function createTables() {
 }
 
 async function rebuildDB() {
-    try {
-      client.connect();
-  
-      await dropTables();
-      await createTables();
-      await createInitialUsers();
-    } catch (error) {
-      throw error;
-    }
-  }
+  try {
+    client.connect();
 
-  async function testDB() {
-    try {
-      console.log("Starting to test database...");
-  
-      console.log("Calling getAllUsers")
-      const users = await getAllUsers();
-      console.log("Result:", users);
-  
-      console.log("Calling updateUser on users[0]")
-      const updateUserResult = await updateUser(users[0].id, {
-        name: "Newname Sogood",
-        location: "Lesterville, KY"
-      });
-      console.log("Result:", updateUserResult);
-  
-      console.log("Finished database tests!");
-    } catch (error) {
-      console.error("Error testing database!");
-      throw error;
-    }
+    await dropTables();
+    await createTables();
+    await createInitialUsers();
+    await createInitialPosts();
+  } catch (error) {
+    throw error;
   }
+}
+
+async function testDB() {
+  try {
+    console.log("Starting to test database...");
+
+    console.log("Calling getAllUsers");
+    const users = await getAllUsers();
+    console.log("Result:", users);
+
+    console.log("Calling updateUser on users[0]");
+    const updateUserResult = await updateUser(users[0].id, {
+      name: "Newname Sogood",
+      location: "Lesterville, KY",
+    });
+    console.log("Result:", updateUserResult);
+
+    console.log("Calling getAllPosts");
+    const posts = await getAllPosts();
+    console.log("Result:", posts);
+
+    console.log("Calling updatePost on posts[0]");
+    const updatePostResult = await updatePost(posts[0].id, {
+      title: "new title"
+    });
+    console.log("Result:", updatePostResult);
+
+    console.log("calling getUserById for users[0]");
+    const getUserByIdResult = await getUserById(users[0].id)
+    console.log("result:",getUserByIdResult )
+
+
+    console.log("Finished database tests!");
+  } catch (error) {
+    console.error("Error testing database!");
+    throw error;
+  }
+}
 
 async function createInitialUsers() {
-    try {
-      console.log("Starting to create users...");
-  
-      const albert = await createUser({ username: 'albert', password: 'bertie99', name: "albert", location:"Nowhere" });
-      
-      const sandra = await createUser({ username: 'sandra', password: '2sandy4me', name: "sandra",location:"Nowhere" });
-    
-      const glamgal = await createUser({ username: 'glamgal', password: 'soglam', name:"glamgal",location:"Nowhere" });
+  try {
+    console.log("Starting to create users...");
 
-      console.log(albert);
-  
-      console.log("Finished creating users!");
-    } catch(error) {
-      console.error("Error creating users!");
-      throw error;
-    }
-  
+    const albert = await createUser({
+      username: "albert",
+      password: "bertie99",
+      name: "albert",
+      location: "Nowhere",
+    });
+
+    const sandra = await createUser({
+      username: "sandra",
+      password: "2sandy4me",
+      name: "sandra",
+      location: "Nowhere",
+    });
+
+    const glamgal = await createUser({
+      username: "glamgal",
+      password: "soglam",
+      name: "glamgal",
+      location: "Nowhere",
+    });
+
+    console.log(albert);
+
+    console.log("Finished creating users!");
+  } catch (error) {
+    console.error("Error creating users!");
+    throw error;
+  }
 }
+
+async function createInitialPosts() {
+  console.log("Starting to create posts...");
+  try {
+    const [albert, sandra, glamgal] = await getAllUsers();
+
+    await createPost({
+      authorId: albert.id,
+      title: "First Post",
+      content:
+        "This is my first post. I hope I love writing blogs as much as I love writing them.",
+    });
+    await createPost({
+      authorId: glamgal.id,
+      title: "my first Post",
+      content: "Ti'm so glam its awesome.",
+    });
+    await createPost({
+      authorId: sandra.id,
+      title: "only post",
+      content: "i don't like to make posts",
+    });
+    await createPost({
+      authorId: albert.id,
+      title: "second Post",
+      content:
+        "This is my second post. I hope I love writing blogs as much as I love writing them.",
+    });
+    await createPost({
+      authorId: glamgal.id,
+      title: "another Post",
+      content: "I hope I love writing blogs as much as I love writing them.",
+    });
+    console.log("Finished creating posts!");
+  } catch (error) {
+    console.log("error creating posts!");
+    throw error;
+  }
+}
+
 rebuildDB()
   .then(testDB)
   .catch(console.error)
